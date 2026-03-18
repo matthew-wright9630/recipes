8000dd7754534a34356b4186667d896b854dfc39
matthew-wright9630 <matthew.wright9630@gmail.com>
2026-03-18 10:57:53 -0400

    Create Cookbook.java model

---
+++ recipe-backend/src/main/java/com/matthew/recipe_backend/models/Cookbook.java
@@ -0,0 +1,104 @@
+package com.matthew.recipe_backend.models;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+public class Cookbook {
+    
+    @Id
+    private long id;
+
+    @Column
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id", nullable = false)
+    private Users owner;
+
+    @Column
+    private boolean deleted;
+
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    public Cookbook() {
+    }
+
+    public Cookbook(String name, Users owner, boolean deleted, Instant createdAt) {
+        this.name = name;
+        this.owner = owner;
+        this.deleted = deleted;
+        this.createdAt = createdAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Users getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Users owner) {
+        this.owner = owner;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cookbook other = (Cookbook) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Cookbook [id=" + id + ", name=" + name + ", deleted=" + deleted + ", createdAt=" + createdAt + "]";
+    }
+}
