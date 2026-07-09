34fdc6f4acc32b8f985ecc3d33fb6c64db6cd3fd
matthew-wright9630 <matthew.wright9630@gmail.com>
2026-07-09 16:51:34 -0400

    Update grid to center cards when only a couple of entries are there

--- recipe-frontend/src/app/shared/dialogs/recipe-create-dialog/recipe-create-dialog.ts
+++ recipe-frontend/src/app/shared/dialogs/recipe-create-dialog/recipe-create-dialog.ts
@@ -1,4 +1,10 @@
-import { Component, inject } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  inject,
+  signal,
+  ViewChild,
+} from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import {
   MatDialog,
@@ -12,6 +18,11 @@ import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { RecipeStateService } from '../../services/recipe-state-service/recipe-state.service';
 import { RecipeEditDialog } from '../recipe-edit-dialog/recipe-edit-dialog';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { UserImageService } from '../../services/user-image-service/user-image.service';
+import { environment } from '../../../../environments/environment';
+import { DEFAULT_RECIPE_IMAGES } from '../../constants/default-images';
 
 @Component({
   selector: 'app-recipe-create-dialog',
@@ -22,6 +33,8 @@ import { RecipeEditDialog } from '../recipe-edit-dialog/recipe-edit-dialog';
     ReactiveFormsModule,
     MatInputModule,
     MatDialogActions,
+    MatButton,
+    MatIcon,
   ],
   templateUrl: './recipe-create-dialog.html',
   styleUrl: './recipe-create-dialog.scss',
@@ -32,29 +45,120 @@ export class RecipeCreateDialog {
   private recipeService = inject(RecipeService);
   private recipeStateService = inject(RecipeStateService);
   private dialog = inject(MatDialog);
+  private imageService = inject(UserImageService);
+
+  userImages: string[] = [];
+  defaultImages = DEFAULT_RECIPE_IMAGES;
+  imageUrl: string = environment.imageBaseUrl + 'recipes/';
 
   form = this.fb.group({
     name: ['', [Validators.minLength(3), Validators.required]],
-    description: ['', [Validators.minLength(2), Validators.required]],
+    description: [''],
+    imageUrl: ['', [Validators.required]],
   });
+
+  selectImage(image: string): void {
+    this.form.get('imageUrl')?.setValue(image);
+  }
+
+  @ViewChild('imageTrack') imageTrack!: ElementRef;
+  currentOffset = 0;
+  readonly SCROLL_AMOUNT = 133; // image width + gap
+  @ViewChild('imageTrack') imageTrackContainer!: ElementRef;
+  maxOffset = signal(100);
+
+  scrollImages(direction: number): void {
+    const container = this.imageTrackContainer.nativeElement;
+    const track = container.querySelector('.recipe-create__image-track');
+    this.maxOffset.set(track.scrollWidth - container.clientWidth);
+
+    this.currentOffset = Math.max(
+      0,
+      Math.min(
+        this.currentOffset + direction * this.SCROLL_AMOUNT,
+        this.maxOffset(),
+      ),
+    );
+    track.style.transform = `translateX(-${this.currentOffset}px)`;
+  }
+
+  selectedImageFile: File | null = null;
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (!input.files || input.files.length === 0) {
+      return;
+    }
+
+    const file = input.files[0];
+
+    if (!file.type.startsWith('image/')) {
+      return;
+    }
+
+    this.selectedImageFile = file;
+
+    this.form.patchValue({
+      imageUrl: null,
+    });
+
+    this.selectedImageFile = file;
+    this.uploadImage();
+  }
+
+  uploadImage(): void {
+    if (!this.selectedImageFile) return;
+
+    const formData = new FormData();
+    formData.append('file', this.selectedImageFile);
+
+    this.imageService.uploadImage(formData).subscribe({
+      next: (baseKey) => {
+        this.form.get('imageUrl')?.setValue(baseKey);
+        this.selectedImageFile = null;
+        this.getListOfImages();
+      },
+      error: (err) => console.error(err),
+    });
+  }
+
+  getImageUrl(baseKey: string, size: 'thumb' | 'medium'): string {
+    return `${environment.imageBaseUrl}/${baseKey}-${size}.jpg`;
+  }
+
+  getListOfImages(): void {
+    this.imageService.getImages().subscribe({
+      next: (images) => {
+        this.userImages = images;
+      },
+      error: (err) => console.error(err),
+    });
+  }
 
   onCancel(): void {
     this.dialogRef.close();
   }
 
   onSave(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
     const draftRecipe: Recipe = {
       ...this.form.getRawValue(),
     } as Recipe;
 
     this.recipeService.createDraftRecipe(draftRecipe).subscribe({
       next: (result) => {
+        console.log(result);
         this.dialogRef.close(result);
         this.dialog.open(RecipeEditDialog, {
           width: '800px',
           maxWidth: '95vw',
           autoFocus: false,
-          data: this.form.getRawValue(),
+          data: result.body,
         });
         this.recipeStateService.notifyRecipeUpdated(result.body as Recipe);
       },
