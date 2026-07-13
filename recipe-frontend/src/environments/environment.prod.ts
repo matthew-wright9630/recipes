67c6546c5efad25cbba7270a5fab52f3663d4ab9
matthew-wright9630 <matthew.wright9630@gmail.com>
2026-07-13 15:31:09 -0400

    Bugfix: fix bug where baseFrontendUrl had a typo

--- recipe-frontend/src/environments/environment.prod.ts
+++ recipe-frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: '',
   imageBaseUrl: 'https://the-wright-kitchen-images.s3.us-east-1.amazonaws.com/',
-  baseFrontendUrl: 'https://www.wright-recipes.com',
+  baseFrontendUrl: 'https://www.wrightrecipes.com',
 };
--- recipe-frontend/src/environments/environment.ts
+++ recipe-frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8083',
   imageBaseUrl: '/uploads',
-  baseFrontendUrl: 'https://www.wright-recipes.com',
+  baseFrontendUrl: 'https://www.wrightrecipes.com',
 };
