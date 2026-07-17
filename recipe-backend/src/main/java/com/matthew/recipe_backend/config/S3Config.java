d8a56c8f5fa758b4dc37789fc9598bce62171326
matthew-wright9630 <matthew.wright9630@gmail.com>
2026-07-17 11:36:29 -0400

    Update styling of recipe-detail and recipe-edit pages. Fix bug where image upload would fail for test environment.

--- recipe-backend/src/main/java/com/matthew/recipe_backend/config/S3Config.java
+++ recipe-backend/src/main/java/com/matthew/recipe_backend/config/S3Config.java
@@ -3,11 +3,13 @@ package com.matthew.recipe_backend.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
+@Profile("!test")
 public class S3Config {
 
     @Bean
